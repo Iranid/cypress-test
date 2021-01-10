ea8fbe25346feeb480c5ffe885d576ff0c8afc73
Iranid Perez <iranidperez@gmail.com>
2021-01-09 23:13:00 -0300

    [test] Add client page tests

--- cypress/integration/selectors/client.js
+++ cypress/integration/selectors/client.js
@@ -1,9 +1,7 @@
 const CLIENT = {
-    SEARCH_INPUT: 'div.SearchInput input',
-    CLIENT_TABLE: '.section-table',
-    CLIENT_TDATA: 'td'
-
+  SEARCH_INPUT: 'div.SearchInput input',
+  CLIENT_TABLE: '.section-table',
+  CLIENT_TDATA: 'td'
 }
-
 
 module.exports.CLIENT = CLIENT
--- cypress/integration/tracking_time_in_a_project_flow.spec.js
+++ cypress/integration/tracking_time_in_a_project_flow.spec.js
@@ -1,10 +1,13 @@
 /// <reference types="cypress" />
 
 const {LOGIN} = require('./selectors/login');
+const {MENU} = require('./selectors/menu');
+const {CLIENT} = require('./selectors/client');
 
 
 describe('Tracking time in a Project', () => {
-  context('before each test using closure variables', () => {
+
+  context('Valid User', () => {
     let app
     let user
 
@@ -12,26 +15,38 @@ describe('Tracking time in a Project', () => {
       cy.fixture('app').then((data) => {
         app = data
       })
-  
+
       cy.fixture('user').then((data) => {
         user = data
       })
     })
-    
-    describe('the login page', () => {
 
-      it('the login is succefull when the email and password are valid', () => {
-        cy.visit(app.url)
+    describe('The login page', () => {
 
+      it('Login is succesful when the email and password are valid', () => {
+        cy.visit(app.login_url)
+
+        // Submit the form
         cy.get(LOGIN.EMAIL).type(user.email)
         cy.get(LOGIN.PASS).type(`${user.pass}{enter}`)
 
+        // Check user is logged-in
         cy.url().should('include', app.main_section_path)
         cy.getCookie(app.logged_cookie_name).should('exist')
       })
+    })
 
-      
+    describe('The Client page', () => {
 
+      it('Validate client "Lemontech" exists', () => {
+
+        // Go to client page
+        cy.get(MENU.ADMIN).click()
+        cy.get(MENU.ADMIN_OPTION_CLIENT).click()
+        cy.url().should('include', app.clients_section_path)
+
+
+      })
     })
   })
 })
